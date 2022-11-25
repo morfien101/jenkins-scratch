fde72a0654b652857f6ea11b42d2d3cb6333208e
Randy Coburn <>
2022-11-25 15:34:32 +0200

    adding first job

---
+++ jdsl/jobs/helloworld1.groovy
@@ -0,0 +1,15 @@
+multibranchPipelineJob('jenkins-scratch-hw1') {
+    displayName("Hello World 1")
+    description("Build job for hello world 1")
+    branchSources {
+        git {
+            id('cc0c52c42e584f1f8081cf50f8e116cf') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/morfien101/jenkins-scratch-hw1')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+}
