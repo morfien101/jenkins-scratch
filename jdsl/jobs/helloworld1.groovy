6a325a5519e4cf70d33724b9b919366935f727dd
Randy Coburn <>
2022-11-25 15:56:50 +0200

    change name

--- jdsl/jobs/helloworld1.groovy
+++ jdsl/jobs/helloworld1.groovy
@@ -1,15 +1,20 @@
-multibranchPipelineJob('jenkins-scratch-hw1') {
-    displayName("Hello World 1")
-    description("Build job for hello world 1")
-    branchSources {
-        git {
-            id('cc0c52c42e584f1f8081cf50f8e116cf') // IMPORTANT: use a constant and unique identifier
-            remote('https://github.com/morfien101/jenkins-scratch-hw1')
-        }
+multibranchPipelineJob('jobs/jenkins-scratch-hw1') {
+  displayName("Hello World 1")
+  description("Build job for hello world 1")
+  branchSources {
+    git {
+      id('cc0c52c42e584f1f8081cf50f8e116cf') // IMPORTANT: use a constant and unique identifier
+      remote('https://github.com/morfien101/jenkins-scratch-hw1')
     }
-    orphanedItemStrategy {
-        discardOldItems {
-            numToKeep(20)
-        }
+  }
+  orphanedItemStrategy {
+    discardOldItems {
+      numToKeep(20)
     }
+  }
+  factory {
+    workflowMultiBranchProjectFactory {
+      scriptPath('my-location/Jenkinsfile')
+    }
+  }
 }
